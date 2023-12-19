a3ba5ab28784d810cef2e9b919e6fac9f68a4dc7
lik3as <daviacosta2006@gmail.com>
2023-12-19 15:02:58 -0300

    initial commit

---
+++ src/components/Box.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+
+const Box: FC = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+
+export default Box;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import { FC } from "react";
+
+interface Props {
+
+};
+
+const Sidebar: FC<Props> = () => {
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
+
+export default Sidebar;
